8fbd49e375dac1644cdf4dbadf3d5937cddbdd5d
Steve Beaudry <live.remix@gmail.com>
2023-01-16 22:02:21 -0500

    new tos and privacy

--- resources/views/tos.blade.php
+++ resources/views/tos.blade.php
@@ -1,95 +1,87 @@
 <x-layout>
-    <h1>Terms and conditions</h1>
-    <p>These terms and conditions (&#8220;Agreement&#8221;) set forth the general terms and conditions of your use of
-        the <a href="https://thegreenasterisk.com">thegreenasterisk.com</a> website (&#8220;Website&#8221; or
-        &#8220;Service&#8221;) and any of its related products and services (collectively, &#8220;Services&#8221;). This
-        Agreement is legally binding between you (&#8220;User&#8221;, &#8220;you&#8221; or &#8220;your&#8221;) and this
-        Website operator (&#8220;Operator&#8221;, &#8220;we&#8221;, &#8220;us&#8221; or &#8220;our&#8221;). If you are
-        entering into this agreement on behalf of a business or other legal entity, you represent that you have the
-        authority to bind such entity to this agreement, in which case the terms &#8220;User&#8221;, &#8220;you&#8221;
-        or &#8220;your&#8221; shall refer to such entity. If you do not have such authority, or if you do not agree with
-        the terms of this agreement, you must not accept this agreement and may not access and use the Website and
-        Services. By accessing and using the Website and Services, you acknowledge that you have read, understood, and
-        agree to be bound by the terms of this Agreement. You acknowledge that this Agreement is a contract between you
-        and the Operator, even though it is electronic and is not physically signed by you, and it governs your use of
-        the Website and Services.</p>
-    <div class="wpembed-index">
-        <h3>Table of contents</h3>
-        <ol class="wpembed-index">
-            <li><a href="#accounts-and-membership">Accounts and membership</a></li>
-            <li><a href="#user-content">User content</a></li>
-            <li><a href="#backups">Backups</a></li>
-            <li><a href="#links-to-other-resources">Links to other resources</a></li>
-            <li><a href="#changes-and-amendments">Changes and amendments</a></li>
-            <li><a href="#acceptance-of-these-terms">Acceptance of these terms</a></li>
-            <li><a href="#contacting-us">Contacting us</a></li>
-        </ol>
-    </div>
-    <h2 id="accounts-and-membership">Accounts and membership</h2>
-    <p>You must be at least 13 years of age to use the Website and Services. By using the Website and Services and by
-        agreeing to this Agreement you warrant and represent that you are at least 13 years of age. If you create an
-        account on the Website, you are responsible for maintaining the security of your account and you are fully
-        responsible for all activities that occur under the account and any other actions taken in connection with it.
-        We may, but have no obligation to, monitor and review new accounts before you may sign in and start using the
-        Services. Providing false contact information of any kind may result in the termination of your account. You
-        must immediately notify us of any unauthorized uses of your account or any other breaches of security. We will
-        not be liable for any acts or omissions by you, including any damages of any kind incurred as a result of such
-        acts or omissions. We may suspend, disable, or delete your account (or any part thereof) if we determine that
-        you have violated any provision of this Agreement or that your conduct or content would tend to damage our
-        reputation and goodwill. If we delete your account for the foregoing reasons, you may not re-register for our
-        Services. We may block your email address and Internet protocol address to prevent further registration.</p>
-    <h2 id="user-content">User content</h2>
-    <p>We do not own any data, information or material (collectively, &#8220;Content&#8221;) that you submit on the
-        Website in the course of using the Service. You shall have sole responsibility for the accuracy, quality,
-        integrity, legality, reliability, appropriateness, and intellectual property ownership or right to use of all
-        submitted Content. We may, but have no obligation to, monitor and review the Content on the Website submitted or
-        created using our Services by you. You grant us permission to access, copy, distribute, store, transmit,
-        reformat, display and perform the Content of your user account solely as required for the purpose of providing
-        the Services to you. Without limiting any of those representations or warranties, we have the right, though not
-        the obligation, to, in our own sole discretion, refuse or remove any Content that, in our reasonable opinion,
-        violates any of our policies or is in any way harmful or objectionable. Unless specifically permitted by you,
-        your use of the Website and Services does not grant us the license to use, reproduce, adapt, modify, publish or
-        distribute the Content created by you or stored in your user account for commercial, marketing or any similar
-        purpose.</p>
-    <h2 id="backups">Backups</h2>
-    <p>We are not responsible for the Content residing on the Website. In no event shall we be held liable for any loss
-        of any Content. It is your sole responsibility to maintain appropriate backup of your Content. Notwithstanding
-        the foregoing, on some occasions and in certain circumstances, with absolutely no obligation, we may be able to
-        restore some or all of your data that has been deleted as of a certain date and time when we may have backed up
-        data for our own purposes. We make no guarantee that the data you need will be available.</p>
-    <h2 id="links-to-other-resources">Links to other resources</h2>
-    <p>Although the Website and Services may link to other resources (such as websites, mobile applications, etc.), we
-        are not, directly or indirectly, implying any approval, association, sponsorship, endorsement, or affiliation
-        with any linked resource, unless specifically stated herein. Some of the links on the Website may be
-        &#8220;affiliate links&#8221;. This means if you click on the link and purchase an item, the Operator will
-        receive an affiliate commission. We are not responsible for examining or evaluating, and we do not warrant the
-        offerings of, any businesses or individuals or the content of their resources. We do not assume any
-        responsibility or liability for the actions, products, services, and content of any other third parties. You
-        should carefully review the legal statements and other conditions of use of any resource which you access
-        through a link on the Website. Your linking to any other off-site resources is at your own risk.</p>
-    <h2 id="changes-and-amendments">Changes and amendments</h2>
-    <p>We reserve the right to modify this Agreement or its terms related to the Website and Services at any time at our
-        discretion. When we do, we will post a notification on the main page of the Website. We may also provide notice
-        to you in other ways at our discretion, such as through the contact information you have provided.</p>
-    <p>An updated version of this Agreement will be effective immediately upon the posting of the revised Agreement
-        unless otherwise specified. Your continued use of the Website and Services after the effective date of the
-        revised Agreement (or such other act specified at that time) will constitute your consent to those changes.</p>
-    <h2 id="acceptance-of-these-terms">Acceptance of these terms</h2>
-    <p>You acknowledge that you have read this Agreement and agree to all its terms and conditions. By accessing and
-        using the Website and Services you agree to be bound by this Agreement. If you do not agree to abide by the
-        terms of this Agreement, you are not authorized to access or use the Website and Services. This terms and
-        conditions policy was created with the help of <a href="https://www.websitepolicies.com" target="_blank"
-            rel="nofollow">WebsitePolicies</a>.</p>
-    <h2 id="contacting-us">Contacting us</h2>
-    <p>If you have any questions, concerns, or complaints regarding this Agreement, we encourage you to contact us using
-        the details below:</p>
-    <p><a
-            href="&#109;&#097;&#105;&#108;&#116;&#111;&#058;st&#101;ve&#64;th&#101;g&#114;ee&#110;a&#115;&#116;&#101;ri&#115;k.co&#109;">stev&#101;&#64;&#116;&#104;&#101;&#103;r&#101;enasterisk&#46;com</a>
-    </p>
-    <p>This document was last updated on January 15, 2023</p>
-    <p class="madewith"><a href="https://www.websitepolicies.com/?via=madewithbadge" target="_blank" rel="nofollow"><img
-                width="200" height="25" alt="Made with WebsitePolicies"
-                src="https://cdn.websitepolicies.io/img/badge.png"
-                srcset="https://cdn.websitepolicies.io/img/badge_2x.png 2x"></a></p>
+    <h1>Terms of Service</h1>
 
+    <p>Welcome to The Green Asterisk (the "Website"). The Website is operated by The Green Asterisk ("we," "us," or
+        "our"). By
+        accessing or using the Website, you agree to be bound by these Terms of Service and our Privacy Policy, whether
+        or not you are a registered user of our Website. If you do not agree to these Terms of Service, you should not
+        use the Website.</p>
+
+    <h2>Accounts</h2>
+    <p>In order to access certain features of the Website, you will have to create an account. You are responsible for
+        maintaining the confidentiality of your account and password and for restricting access to your computer, and
+        you agree to accept responsibility for all activities that occur under your account or password. We reserve the
+        right to refuse service, terminate accounts, or remove or edit content in our sole discretion.</p>
+
+    <h2>Content</h2>
+    <p>The Website may contain text, software, scripts, graphics, photos, sounds, music, videos, audiovisual
+        combinations, interactive features and other materials (collectively, "Content"). The Content may be owned by us
+        or by third parties. The Content is protected by copyright and other intellectual property laws.</p>
+
+    <p>You may access the Content for your information and personal use solely as intended through the provided
+        functionality of the Website and as permitted under these Terms of Service. You shall not download any Content
+        unless you see a “download” or similar link displayed by us on the Website for that Content. You shall not copy,
+        reproduce, distribute, transmit, broadcast, display, sell, license, or otherwise exploit any Content for any
+        other purposes without the prior written consent of the respective owners. We reserve all rights not expressly
+        granted in and to the Website and the Content.</p>
+
+    <h2>Prohibited Uses</h2>
+    <p>You may use the Website only for lawful purposes and in accordance with these Terms of Service. You agree not to
+        use the Website:</p>
+    <ul>
+        <li>In any way that violates any applicable federal, state, local, or international law or regulation.</li>
+        <li>For the purpose of exploiting, harming, or attempting to exploit or harm minors in any way by exposing them
+            to inappropriate content, asking for personally identifiable information, or otherwise.</li>
+        <li>To transmit, or procure the sending of, any advertising or promotional material, including any "junk mail,"
+            "chain letter," "spam," or any other similar solicitation.</li>
+        <li>To impersonate or attempt to impersonate The Green Asterisk, a The Green Asterisk employee, another user, or
+            any
+            other person or entity.</li>
+        <li>To engage in any other conduct that restricts or inhibits anyone's use or enjoyment of the Website, or
+            which, as determined by us, may harm The Green Asterisk or users of the Website or expose them to liability.
+        </li>
+    </ul>
+
+    <h2>Intellectual Property</h2>
+    <p>The Website and its original content, features and functionality are and will remain the exclusive property of
+        The Green Asterisk and its licensors. The Website is protected by copyright, trademark, and other laws of both
+        the
+        United States and foreign countries. Our trademarks and trade dress may not be used in connection with any
+        product or service without the prior written consent of The Green Asterisk.</p>
+
+    <h2>Links To Other Websites</h2>
+    <p>Our Website may contain links to third-party websites or services that are not owned or controlled by The Green
+        Asterisk. The Green Asterisk has no control over, and assumes no responsibility for, the content, privacy
+        policies, or practices of any third party websites or services. You further acknowledge and agree that The Green
+        Asterisk shall not be responsible or liable, directly or indirectly, for any damage or loss caused or alleged to
+        be caused by or in connection with use of or reliance on any such content, goods or services available on or
+        through any such websites or services.</p>
+
+    <p>We strongly advise you to read the terms and conditions and privacy policies of any third-party websites or
+        services that you visit.</p>
+
+    <h2>Termination</h2>
+    <p>We reserve the right to terminate or suspend your access to the Website immediately, without prior notice or
+        liability, for any reason whatsoever, including without limitation if you breach the Terms of Service.</p>
+
+    <p>All provisions of the Terms of Service which by their nature should survive termination shall survive
+        termination, including, without limitation, ownership provisions, warranty disclaimers, indemnity and
+        limitations of liability.</p>
+
+    <h2>Governing Law</h2>
+    <p>These Terms of Service and any separate agreements whereby we provide you Services shall be governed by and
+        construed in accordance with the laws of Hillsborough County, Florida, USA.</p>
+
+    <h2>Changes To The Terms of Service</h2>
+    <p>We reserve the right, at our sole discretion, to modify or replace these Terms of Service by posting the updated
+        terms on the Website. Your continued use of the Website after any such changes constitutes your acceptance of
+        the new Terms of Service.</p>
+
+    <p>Please review this Agreement periodically for changes. If you do not agree to any of this Agreement or any
+        changes to this Agreement, do not use, access or continue to access the Website or discontinue any use of the
+        Website immediately.</p>
+
+    <h2>Contact Us</h2>
+    <p>If you have any questions about these Terms of Service, please contact us at <a
+            href="mailto:info@thegreenasterisk.com">info@thegreenasterisk.com</a>.</p>
 </x-layout>
