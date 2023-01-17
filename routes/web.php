ce9fbca2f86ce7b3fd22b636cf6dbb105a6fedad
Steve Beaudry <live.remix@gmail.com>
2023-01-17 02:45:27 -0500

    ...

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    if (env('APP_ENV') === 'production' && ! auth()->user()->isAdmin()) {
+    if (env('APP_ENV') === 'production' && ! Auth::user()->isAdmin()) {
         return redirect('https://thegreenasterisk.netlify.app/');
     } else {
         return view('welcome');
