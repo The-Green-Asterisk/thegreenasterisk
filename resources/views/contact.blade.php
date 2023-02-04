13f534948c4d0a88aabbad1cdf9c701d1fa29fb3
Steve Beaudry <live.remix@gmail.com>
2023-02-04 13:15:32 -0500

    ...

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -17,11 +17,10 @@
             <textarea class="form-control" id="message" name="message" rows="5" required
                 placeholder="Write whatever you want here, but if it's rude I reserve the right to ignore it."></textarea>
         </div>
-
-        <script src="https://www.google.com/recaptcha/enterprise.js?render=6Le6IFEkAAAAAFUMdtlncJ34CLU-kvRYXAsmODA9"></script>
+        <script src="https://www.google.com/recaptcha/enterprise.js?render=6Lf8XVAkAAAAAFm2UBRv7vkP8Fh6MPGBqQ-XW6Kh"></script>
         <script>
             grecaptcha.enterprise.ready(function() {
-                grecaptcha.enterprise.execute('6Le6IFEkAAAAAFUMdtlncJ34CLU-kvRYXAsmODA9', {
+                grecaptcha.enterprise.execute('6Lf8XVAkAAAAAFm2UBRv7vkP8Fh6MPGBqQ-XW6Kh', {
                     action: 'login'
                 }).then(function(token) {
                     document.getElementById('g-recaptcha-response').value = token;
@@ -29,8 +28,6 @@
                 });
             });
         </script>
-        <div class="g-recaptcha" data-sitekey="6Le6IFEkAAAAAFUMdtlncJ34CLU-kvRYXAsmODA9"></div>
-        <script src="https://www.google.com/recaptcha/api.js" async defer></script>
         <div class="button-row">
             <button type="submit" class="btn btn-primary" disabled>Send</button>
         </div>
