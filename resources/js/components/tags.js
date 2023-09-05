33fa509ac61579bc64a953a37b5dfd126bc7ce3f
Steve Beaudry <live.remix@gmail.com>
2023-09-05 15:52:42 -0400

    oops

--- resources/js/components/tags.js
+++ resources/js/components/tags.js
@@ -7,7 +7,7 @@ export default function (el) {
                 const modal = document.createElement('div');
                 modal.innerHTML = html;
                 el.body.appendChild(modal);
-                modal.querySelector('#submit-modal') = function () {
+                modal.querySelector('#submit-modal').onclick = function () {
                     let name = el.tagNameInput.value;
                     post('/tag', {name: name})
                         .then(window.location.reload());
