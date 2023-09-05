29cc5c96b8d1b9dbd7903b60fcdba2f81fa687ee
Steve Beaudry <live.remix@gmail.com>
2023-09-05 16:07:30 -0400

    Look out for more fuckups!

--- resources/js/components/modal.js
+++ resources/js/components/modal.js
@@ -2,12 +2,14 @@ export default function (el) {
     el.grabModal();
     if (!el.modal) return;
 
-    el.modal.querySelector('#close-modal').onclick = function () {
-        el.modal.remove();
-    }
+    el.modal.querySelectorAll('#close-modal').forEach(button => {
+        button.onclick = function () {
+            el.modal.remove();
+        }
+    });
     el.modal.onclick = function (event) {
         if (event.target.id === el.modal.id) {
             el.modal.remove();
         }
-    }
+    };
 }
