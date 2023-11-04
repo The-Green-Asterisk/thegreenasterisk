3196d42286b5b0c194301a43ae353dff8795cee0
Steve Beaudry <live.remix@gmail.com>
2023-11-04 14:48:09 -0400

    ...

--- routes/web.php
+++ routes/web.php
@@ -37,7 +37,7 @@ Route::post('update', function (Request $request) {
 
     file_put_contents(
         'update.txt',
-        shell_exec('.././update.sh')
+        shell_exec('sudo .././update.sh')
     );
 
     return response('OK', 200);
