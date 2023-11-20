e97c9a1ad4145ad730629e7013900fe34be54654
Steve Beaudry <live.remix@gmail.com>
2023-11-20 11:36:50 -0500

    make login tokens longer

---
+++ database/migrations/2023_11_20_163252_make_login_tokens_longer.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('login_service_token', 1000)->nullable()->change();
+            $table->string('login_service_refresh_token', 1000)->nullable()->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('login_service_token')->nullable()->change();
+            $table->string('login_service_refresh_token')->nullable()->change();
+        });
+    }
+};
