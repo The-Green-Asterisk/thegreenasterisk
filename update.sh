a378e2dc4f2bd26bb48c43d9911f0b56d3080828
Steve Beaudry <live.remix@gmail.com>
2023-12-22 18:23:56 -0500

    another update

--- update.sh
+++ update.sh
@@ -1,3 +1,4 @@
+echo $(date +"%Y-%m-%d %T")
 cd /usr/local/var/www/thegreenasterisk
 git pull origin main
 composer install --no-dev --no-interaction --prefer-dist --optimize-autoloader
@@ -5,3 +6,4 @@ php artisan migrate --force
 npm run build
 php artisan cache:clear
 php artisan route:cache
+echo $(date +"%Y-%m-%d %T")
