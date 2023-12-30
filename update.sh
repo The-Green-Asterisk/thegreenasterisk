c1fd6636eeaf2b8d46020afcd68c0c743f27db47
Steve Beaudry <live.remix@gmail.com>
2023-12-30 00:54:17 -0500

    blah

--- update.sh
+++ update.sh
@@ -1,9 +1,9 @@
-date > /public/update.txt
-cd /usr/local/var/www/thegreenasterisk >> /public/update.txt
-git pull origin main >> /public/update.txt
-composer install --no-dev --no-interaction --prefer-dist --optimize-autoloader >> /public/update.txt
-php artisan migrate --force >> /public/update.txt
-npm run build >> /public/update.txt
-php artisan cache:clear >> /public/update.txt
-php artisan route:cache >> /public/update.txt
-date >> /public/update.txt
+date > public/update.txt
+cd /usr/local/var/www/thegreenasterisk >> public/update.txt
+git pull origin main >> public/update.txt
+composer install --no-dev --no-interaction --prefer-dist --optimize-autoloader >> public/update.txt
+php artisan migrate --force >> public/update.txt
+npm run build >> public/update.txt
+php artisan cache:clear >> public/update.txt
+php artisan route:cache >> public/update.txt
+date >> public/update.txt
